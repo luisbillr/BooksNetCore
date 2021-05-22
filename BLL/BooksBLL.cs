23d6a2eca7625d5fd22baa68d5c254a3839d8928
LUIS BILL HESKEY RODRIGUEZ <luisbillr@hotmail.com>
2021-05-22 19:47:02 -0400

    Add Api

---
+++ BLL/BooksBLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http.Results;
+using BLL.Entities;
+using Newtonsoft.Json;
+namespace BLL
+{
+ 
+    public class BooksBLL
+    {
+        private static string BASEAPIUrl { get; set; } = "https://fakerestapi.azurewebsites.net";
+        private static HttpClient HttpClient = new HttpClient();
+        //public static async IEnumerable<Book> GetBooks()
+        //{
+        //   List<Book> books = new List<Book>();
+        //   HttpResponseMessage url =  await HttpClient.GetAsync( BASEAPIUrl + "/api/v1/Books");
+        //   string respuesta = await url.Content.ReadAsStringAsync();
+        //   var datos = JsonConvert.DeserializeObject<IEnumerable<Book>>(respuesta);
+          
+        //}
+    }
+}
---
+++ BLL/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Entities
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int pageCount { get; set; }
+        public string excerpt { get; set; }
+        public DateTime publishDate { get; set; }
+    }
+}
